55e95609651da17469aebc771627b3a4fc49215e
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-10-11 23:03:17 -0500

    Navbar, with cart icon

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -1,8 +1,30 @@
+import Stripe from "stripe"
 
-export default function CoffeePage() {
-    return (
-        <div>
-            <h1>Coffee Page</h1>
-        </div>
-    )
+async function getStripeProducts() {
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
+    apiVersion: '2023-08-16'
+  })
+  const res = await stripe.prices.list({
+    expand: ['data.product']
+  })
+  const prices = res.data
+  return prices
+}
+
+export default async function CoffeePage() {
+  const products = await getStripeProducts();
+  //console.log(products)
+  return (
+    <ul>
+      {products && products.map((item: any, i) => (
+        <li key={i}>
+          <span className="border-red-300 mr-4 border">{item.product.name}</span>
+          <span>{item.product.description}</span><br />
+          <img src={item.product.images[0]} /><br />
+          <span>{item.unit_amount / 100}</span>
+        </li>
+      ))}
+    </ul>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,8 @@
-import Stripe from "stripe"
 
-async function getStripeProducts() {
-
-  const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
-    apiVersion: '2023-08-16'
-  })
-  const res = await stripe.prices.list({
-    expand: ['data.product']
-  })
-  const prices = res.data
-  return prices
-}
-
-export default async function Home() {
-  const products = await getStripeProducts();
-  //console.log(products)
+export default function Home() {
   return (
-    <ul>
-      {products && products.map((item: any, i) => (
-        <li key={i}>
-          <span className="border-red-300 mr-4 border">{item.product.name}</span>
-          <span>{item.product.description}</span><br />
-          <img src={item.product.images[0]} /><br />
-          <span>{item.unit_amount / 100}</span>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h1>Home Page</h1>
+    </div>
   )
 }
