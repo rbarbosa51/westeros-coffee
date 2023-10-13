50106e282b90c9d79ebb91e1d888a3ace24f7baa
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-10-13 18:37:03 -0500

    Home page complete

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -11,13 +11,9 @@ export default function TeamPage() {
                     <h2 className="card-title">Daenerys Targaryen</h2>
                     <p>Daenerys Stormborn of House Targaryen, the First of Her Name, Queen of the Andals and the First Men, Protector of the Seven Kingdoms, the Mother of Dragons, the Khaleesi of the Great Grass Sea, the Unburnt, the Breaker of Chains.</p>
                     <p className="font-bold">and now... CEO of Westeros Coffee!!!</p>
-                    {/* <div className="card-actions justify-end">
-                      <button className="btn btn-primary">Learn now!</button>
-                    </div> */}
                 </div>
             </div>
             {/* Our Team */}
-            {/* <h2 className="text-primary text-center text-xl lg:text-2xl mt-2">Meet our Team</h2> */}
             <div className="card glass lg:card-side p-2 lg:p-4 mt-4 shadow-lg">
                 <figure><img src="/BoardMembers.jpeg" className="rounded-xl " /></figure>
                 <div className="card-body ">
@@ -55,8 +51,7 @@ export default function TeamPage() {
                 <div className="card glass p-2 lg:p-4 w-[45%] shadow-lg">
                     <figure><img src="/dragon1.jpeg" className="rounded-xl " /></figure>
                     <div className="card-body">
-                        {/* <h2 className="card-title text-center">Board Members</h2> */}
-                        <p>Our coffee is brewed with the help of Dragon Fire. Dragon Fire ensures the highest quality.</p>
+                        <p>Our coffee is roasted with the help of Dragon Fire. Dragon Fire ensures the highest quality.</p>
                     </div>
                 </div>
                 <div className="divider divider-vertical lg:divider-horizontal ">Or</div>
@@ -67,7 +62,7 @@ export default function TeamPage() {
                     </div>
                 </div>
             </div>
-            {/* <div className='divider mt-2 w-[90%] mx-auto'></div> */}
+            
         </div>
     )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -16,7 +16,7 @@ export default function NavBar() {
     ]
     return (
         <nav className="navbar flex justify-between mb-2 bg-base-100/50">
-            {/* Hambrger */}
+            {/* Hamburger */}
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
