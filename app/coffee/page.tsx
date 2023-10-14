9eb894d001aa0ccebc910115b723dff1f683ce48
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-10-14 15:58:02 -0500

    using localeString for dollar amount

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -21,11 +21,13 @@ export default function CoffeePage() {
   
   return (
     <div className="flex flex-wrap gap-8 justify-center mt-4 lg:mt-8 ">
-      {prices && prices.map((item: any) => (
+      {prices ? prices.map((item: any) => (
         <CoffeeCard key={item.id} id={item.id} name={item.product.name} 
         description={item.product.description} image={item.product.images[0]} 
         amount={item.unit_amount / 100}/>
-      ))}
+      )) :
+        <h1 className="text-accent-focus text-3xl lg:text-4xl text-center">Loading Content</h1>
+      }
       
     </div>
     
--- components/CoffeeCard.tsx
+++ components/CoffeeCard.tsx
@@ -9,7 +9,7 @@ interface iProps {
 }
 
 export default function CoffeeCard({id, name, description, image, amount}: iProps) {
-    const [currentAmount, setCurrentAmount] = useState<number>(0)
+    const [currentQuantity, setCurrentQuantity] = useState<number>(0)
     return (
         <div className="card glass shadow-lg w-72">
             <div className="card-body">
@@ -18,13 +18,13 @@ export default function CoffeeCard({id, name, description, image, amount}: iProp
                 <p>{description}</p>
                 <div className="flex gap-4 items-center">
                     <div>Price:</div>
-                    <div className="badge badge-accent badge-outline">{amount}</div>
+                    <div className="badge badge-accent badge-outline">{amount.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</div>
                 </div>
                 <div className="divider divider-vertical"></div>
                 <div className="flex justify-center -mt-4">
-                    <button className="text-2xl btn" onClick={() => setCurrentAmount(prev => prev < 99 ? prev + 1: prev)}>+</button>
-                    <input type="text" readOnly value={currentAmount} className="input input-ghost w-14 text-center" />
-                    <button className="text-2xl btn" onClick={() => setCurrentAmount(prev => prev > 0 ? prev - 1: prev)}>-</button>
+                    <button className="text-2xl btn" onClick={() => setCurrentQuantity(prev => prev < 99 ? prev + 1: prev)}>+</button>
+                    <input type="text" readOnly value={currentQuantity} className="input input-ghost w-14 text-center" />
+                    <button className="text-2xl btn" onClick={() => setCurrentQuantity(prev => prev > 0 ? prev - 1: prev)}>-</button>
                 </div>
                 <button className="btn btn-primary">Add to Cart</button>
             </div>
