b31759a266e1e39a641d04e46f20e9a23dd2335d
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-10-14 14:40:52 -0500

    Created success and cancel pages

---
+++ app/cancel/page.tsx
@@ -0,0 +1,12 @@
+
+export default function CancelPage() {
+    return (
+        <div>
+            <h1 className="text-primary text-center text-3xl lg:text-4xl">Something was wrong.</h1>
+            <div className="mt-4 lg:mt-8">
+                <img src="/cancel.jpeg" alt="Success Image" className="lg:w-128 mx-auto" />
+            </div>
+            <p className="text-xl lg:text-2xl text-center mt-4 lg:mt-8">There was a problem with your transaction. </p>
+        </div>
+    )
+}
