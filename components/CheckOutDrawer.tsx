ccc0e74b422ca41b7156dc5037eb3a1cfac5d253
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-10-14 23:51:21 -0500

    Checkout drawer partially finished

--- components/CheckOutDrawer.tsx
+++ components/CheckOutDrawer.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useStore } from "@/app/(store)/store"
-//import { useState } from "react"
+
 interface iProp {
     open?: boolean
     setOpen?: () => void
@@ -15,17 +15,34 @@ export default function CheckOutDrawer({open, setOpen}:iProp) {
         setOpen(prev => !prev)
     }
     return (
-        <div className={`card ${open ? 'flex' : 'hidden'} h-96 w-96 absolute z-30 shadow-xl right-0 top-2 glass`}>
-            <h2 className="card-title text-primary">Checkout</h2>
+        <div className={`card ${open ? 'flex' : 'hidden'} min-h-96 w-96 absolute z-30 shadow-xl right-0 top-2 bg-base-200`}>
+            <div className="flex justify-between w-full mx-10">
+                <h2 className="card-title text-primary text-3xl lg:text-4xl">Checkout</h2>
+                <button className="btn btn-square w-4 aspect-square" onClick={handleClick}>X</button>
+            </div>
             <ul>
-                {transaction && transaction.map((price, i) => {
-                    console.log(price.name)
-                    return <li key={i}>{price.name}</li>
-                })}
+                {(transaction.length > 0) ? transaction.map((price, i) => (
+                    <div key={i} className="flex flex-col font-bold mx-auto w-[90%]">
+                        <div className="flex w-full gap-24 justify-between items-center">
+                            <div className="w-[40%]">{price.name}</div>
+                            <div className="">Quantity: {price.quantity}</div>
+                        </div>
+                        <div className="flex w-full gap-52 justify-between items-center">
+                            <div>Total:</div>
+                            <div>{price.price * price.quantity}</div>
+                        </div>
+                        <div className="divider -mt-2"></div>
+                    </div>
+                )) :
+                (
+                    <h1 className="text-2xl lg:text-3xl text-accent my-[50%]">Cart is Empty</h1>
+                )
+                
+                }
             </ul>
             <div className="card-actions">
                 {/* @ts-ignore */}
-                <button className="btn btn-primary" onClick={handleClick}>Click</button>
+                <button className="btn btn-primary" onClick={handleClick}>Proceed to Checkout</button>
             </div>
         </div>
     )
